5c54fe0d15377bdda6daed667a063ae14cc0a799
Asfia Numani <asfi_numani@hotmail.com>
2015-01-15 19:47:36 -0700

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,9 +1,8 @@
 // create class CardDeck that extends CardList
-public CardDeck extends CardList
+public class CardDeck extends CardList
 {
   private int suits;
   private int ranks;
-  private int swaps=10,000;
   public CardDeck()
   {
     Card temp;
@@ -16,3 +15,14 @@ public CardDeck extends CardList
       }
     }
   }
+  public void Shuffle()
+  {
+    for(int count=0; count<10000; count++)
+    {
+      Card card=removeRandomCard();
+      addCardToTop(Card card);
+      Card card2=removeRandomCard();
+      addCardToBottom(Card card2);
+    }
+  }
+}
