49f0e654e824563dd05f9b6eba252be9ed7f80c2
Asfia Numani <asfi_numani@hotmail.com>
2015-01-16 15:11:51 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -9,20 +9,96 @@ public class Card extends PlayingCard implements Comparable<Card>
   {
    if (this.getSuit()==1)
    {
-     return this.getRank()+"-"+"H";
+    if(this.getRank()==10)
+    {
+      return "H-T";
+    }
+    else if (this.getRank()==11)
+    {
+      return "H-J";
+    }
+    else if(this.getRank()==12)
+    {
+      return "H-Q";
+    }
+    else if(this.getRank()==13)
+    {
+      return "H-K";
+    }
+    else
+    {
+      return "H-"+this.getRank();
+    }
    }
-   else if (this.getSuit()==2)
+     if (this.getSuit()==2)
    {
-     return this.getRank()+"-"+"C";
-   }
-   else if (this.getSuit()==3)
+    if(this.getRank()==10)
+    {
+      return "C-T";
+    }
+    else if (this.getRank()==11)
+    {
+      return "C-J";
+    }
+    else if(this.getRank()==12)
+    {
+      return "C-Q";
+    }
+    else if(this.getRank()==13)
+    {
+      return "C-K";
+    }
+    else
+    {
+      return "C-"+this.getRank();
+    }
+     }
+     if (this.getSuit()==3)
    {
-     return this.getRank()+"-"+"S";
-   }
-   else
+    if(this.getRank()==10)
+    {
+      return "S-T";
+    }
+    else if (this.getRank()==11)
+    {
+      return "S-J";
+    }
+    else if(this.getRank()==12)
+    {
+      return "S-Q";
+    }
+    else if(this.getRank()==13)
+    {
+      return "S-K";
+    }
+    else
+    {
+      return "S-"+this.getRank();
+    }
+     }
+    else
    {
-     return this.getRank()+"-"+"D";
-   }
+    if(this.getRank()==10)
+    {
+      return "D-T";
+    }
+    else if (this.getRank()==11)
+    {
+      return "D-J";
+    }
+    else if(this.getRank()==12)
+    {
+      return "D-Q";
+    }
+    else if(this.getRank()==13)
+    {
+      return "D-K";
+    }
+    else
+    {
+      return "D-"+this.getRank();
+    }
+     }
      
    }
   public int compareTo(Card other)
